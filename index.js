38a111ba81f8be1075f1cf939ab445a6ea63b268
Anton Shumsky <anton.shumsky@gmail.com>
2017-06-01 13:01:48 +0300

    implement masking of digits inside strings and numbers

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const _ = require('lodash');
 
-module.exports = function jsonMask(target) {
+module.exports = (target) => {
   return _.cloneDeepWith(target, (value) => {
     if (typeof(value) === 'string' || value instanceof String) {
       return maskString(value);
+    }
+    if (typeof(value) === 'number' || value instanceof Number) {
+      return maskNumber(value);
     }
   });
 };
 
 const upperCaseLetters = /[A-Z]/g;
 const lowerCaseLetters = /[a-z]/g;
+const digits = /\d/g;
 
 const maskString = (value) => {
-  return value.replace(upperCaseLetters, 'X').replace(lowerCaseLetters, 'x');
+  return value.replace(upperCaseLetters, 'X').replace(lowerCaseLetters, 'x').replace(digits, '*');
 }
+
+const maskNumber = () => 12345;
--- test.js
+++ test.js
@@ -2,7 +2,15 @@ const mask = require('./index');
 const {assert} = require('chai');
 
 describe('json-mask', () => {
-  it('should mask strings with X characters', () => {
+  it('should mask letters in a string with X characters', () => {
     assert.deepEqual(mask({name: 'John Doe'}), {name: 'Xxxx Xxx'});
+  });
+  
+  it('should mask digits in a string with * characters', () => {
+    assert.deepEqual(mask({phone: '+1-313-85-93-62'}), {phone: '+*-***-**-**-**'});
+  });
+  
+  it('should mask numbers with 12345', () => {
+    assert.deepEqual(mask({age: 64}), {age: 12345});
   });
 });
