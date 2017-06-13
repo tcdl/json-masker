a0214d8e685d5571b1944e9e8ecebc76e96185ad
Anton Shumsky <anton.shumsky@gmail.com>
2017-06-13 16:50:33 +0300

    add benchmarks

---
+++ benchmark/benchmark.js
@@ -0,0 +1,22 @@
+const Benchmark = require('benchmark');
+const fs = require('fs');
+
+const mask = require('../index');
+
+console.log('Sample\t\tSize\tExecutions/sec\t\tAvg exec time (ms)');
+
+[
+  './sample0.json',
+  './sample1.json',
+  './sample2.json',
+  './sample3.json',
+  './sample4.json'
+  
+].forEach((file) => {
+  const size = Math.ceil(fs.statSync(file).size / 1000);
+  new Benchmark(file, () => mask(require(file)), {
+    onComplete: function () {
+      console.log(`${this.name}\t${size}k\t${this.hz}\t${1000 / this.hz}`);
+    }
+  }).run();
+});
