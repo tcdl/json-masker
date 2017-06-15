cf25e3291c723e5efaaa0871ddc61b8a7a9ae3d3
Anton Shumsky <anton.shumsky@gmail.com>
2017-06-15 17:40:15 +0300

    align samples in benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -9,14 +9,13 @@ console.log('Sample\t\tSize\tExecutions/sec\t\tAvg exec time (ms)');
   './sample0.json',
   './sample1.json',
   './sample2.json',
-  './sample3.json',
-  './sample4.json'
+  './sample3.json'
   
 ].forEach((file) => {
   const size = Math.ceil(fs.statSync(file).size / 1000);
   new Benchmark(file, () => mask(require(file)), {
     onComplete: function () {
-      console.log(`${this.name}\t${size}k\t${this.hz}\t${1000 / this.hz}`);
+      console.log(`${this.name}\t${size}kb\t${this.hz}\t${1000 / this.hz}`);
     }
   }).run();
 });
