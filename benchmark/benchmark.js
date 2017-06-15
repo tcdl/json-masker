1637697d97050728726e17f23992562229e724bf
Anton Shumsky <anton.shumsky@gmail.com>
2017-06-15 17:52:32 +0300

    require samples before test

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -10,10 +10,11 @@ console.log('Sample\t\tSize\tExecutions/sec\t\tAvg exec time (ms)');
   './sample1.json',
   './sample2.json',
   './sample3.json'
-  
+
 ].forEach((file) => {
   const size = Math.ceil(fs.statSync(file).size / 1000);
-  new Benchmark(file, () => mask(require(file)), {
+  const sample = require(file);
+  new Benchmark(file, () => mask(sample), {
     onComplete: function () {
       console.log(`${this.name}\t${size}kb\t${this.hz}\t${1000 / this.hz}`);
     }
