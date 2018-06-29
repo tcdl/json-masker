e815149eb40e6962b9bfd2c486721b98ff5b1d87
Anton Shumsky <anton.shumsky@gmail.com>
2018-06-29 12:58:32 +0300

    update benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,19 +1,20 @@
 const Benchmark = require('benchmark');
 const fs = require('fs');
 
-const mask = require('../masker');
+const masker = require('../masker');
 
 console.log('Sample\t\tSize\tExecutions/sec\t\tAvg exec time (ms)');
 
 [
-  './sample0.json',
-  './sample1.json',
-  './sample2.json',
-  './sample3.json'
+  ['./sample0.json', ['html_url', 'received_events_url', '$..diff_url', '$[0].url']],
+  ['./sample1.json', ['state', 'maintainer_can_modify', '$..href', '$.title']],
+  ['./sample2.json', ['sha', '$..name', '$[0].commit.url']],
+  ['./sample3.json', ['customer.additional.inputDeliveryPointSuffix', '$..urn']]
 
-].forEach((file) => {
+].forEach(([file, whitelist]) => {
   const size = Math.ceil(fs.statSync(file).size / 1024);
   const sample = require(file);
+  const mask = masker({whitelist});
   new Benchmark(file, () => mask(sample), {
     onComplete: function () {
       console.log(`${this.name}\t${size}kB\t${this.hz}\t${1000 / this.hz}`);
